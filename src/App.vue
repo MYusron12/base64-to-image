a00dfc772e97a5e12014fe8514e79a2b9d589ac0
supardiputra <93508603+pardiputram@users.noreply.github.com>
2024-05-01 10:49:54 +0700

    add dist

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
-  <div>
-    <textarea v-model="base64String" placeholder="Masukkan base64" class="form-control" rows="5"></textarea>
+  <div class="containe">
+    <nav class="navbar bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          Base64 to image
+        </a>
+      </div>
+    </nav>
+  </div>
+  <div class="container">
+    <div class="mb-3">
+      <label for="exampleFormControlTextarea1" class="form-label">Input Base64</label>
+      <textarea v-model="base64String" class="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
+    </div>
     <div class="text-center" style="margin-top: 10px;">
       <button @click="displayImage" class="btn btn-primary">Tampilkan Gambar</button>
     </div>
-    <br>
-    <img v-if="imageUrl" :src="imageUrl" alt="Gambar" class="img-fluid mx-auto d-block"
-      style="max-width: 100%; height: auto;">
+    <div>
+      <div class="card text-bg-white mt-5">
+        <img v-if="imageUrl" :src="imageUrl" alt="Gambar" class="img-fluid mx-auto d-block">
+      </div>
+    </div>
   </div>
 </template>
 
